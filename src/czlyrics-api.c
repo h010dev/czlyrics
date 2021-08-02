3e1964b38d43fc42ae43c18a717caaa6515fec3c
MohamedAl-Hussein <sn95racing.v6@gmail.com>
2021-08-02 15:01:43 -0400

    Implement spider call demo in main

--- src/czlyrics-api.c
+++ src/czlyrics-api.c
@@ -2,7 +2,10 @@
 
 #include "external/mongoose/mongoose.h"
 
+#include "czlyrics-spider.h"
+
 static const char *s_lyrics_endpoint = "/api/lyrics/*/*";
+static int         err;
 
 void
 fn (struct mg_connection *c, int ev, void *ev_data, void *fn_data)
@@ -13,7 +16,14 @@ fn (struct mg_connection *c, int ev, void *ev_data, void *fn_data)
 
         if (mg_http_match_uri (hm, s_lyrics_endpoint))
         {
-            mg_http_reply (c, 200, "", "{\"result\": \"%.*s\"}\n", (int) hm->uri.len, hm->uri.ptr);
+            if ((err = scrape_lyrics (s_lyrics_endpoint)) == 0)
+            {
+                mg_http_reply (c, 200, "", "200: OK");
+            }
+            else 
+            {
+                mg_http_reply (c, 404, "", "404: Not found");
+            }
         }
         else
         {

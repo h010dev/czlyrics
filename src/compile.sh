ba905f20297cecea721ae8f9e446d1f1ad76cd7e
MohamedAl-Hussein <sn95racing.v6@gmail.com>
2021-08-06 13:37:41 -0400

    Create Makefile for project to replace bash script

--- src/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-gcc -c czlyrics-spider.c;
-gcc -c czlyrics-api.c;
-gcc czlyrics-api.o czlyrics-spider.o main.c external/mongoose/mongoose.c external/mjson/mjson.c -I external/mongoose -I external/mjson -DMG_ENABLE_OPENSSL=1 -I /usr/include/openssl -L /usr/lib/ -lssl -lcrypto;
-rm *.o;

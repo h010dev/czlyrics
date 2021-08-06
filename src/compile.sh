49287b5c4eab316065e722f76e581c35ba683d49
MohamedAl-Hussein <sn95racing.v6@gmail.com>
2021-08-06 03:38:25 -0400

    Add SSL support to scraper
    Songlyrics website had broken format, so switched to azlyrics which requires https.

--- src/compile.sh
+++ src/compile.sh
@@ -2,5 +2,5 @@
 
 gcc -c czlyrics-spider.c;
 gcc -c czlyrics-api.c;
-gcc czlyrics-api.o czlyrics-spider.o main.c external/mongoose/mongoose.c external/mjson/mjson.c -I external/mongoose -I external/mjson;
+gcc czlyrics-api.o czlyrics-spider.o main.c external/mongoose/mongoose.c external/mjson/mjson.c -I external/mongoose -I external/mjson -DMG_ENABLE_OPENSSL=1 -I /usr/include/openssl -L /usr/lib/ -lssl -lcrypto;
 rm *.o;

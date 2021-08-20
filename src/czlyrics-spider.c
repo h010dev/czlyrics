bc62fe77a75ee9c0e1f2fa60a987b928ebea2e6d
MohamedAl-Hussein <sn95racing.v6@gmail.com>
2021-08-19 22:00:06 -0400

    Refactor spider methods
    Create new struct to hold params to pass to callback handler.
    Create constructor/destructor for new struct.
    Create constant variables to hold common strings.

--- src/czlyrics-spider.c
+++ src/czlyrics-spider.c
@@ -2,74 +2,120 @@
 
 #include "czlyrics-spider.h"
 
-static const char *s_user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36";
-static const char *s_host       = "www.azlyrics.com";
-static const char *s_artist;
-static const char *s_song;
-static int         err;
+const char* const USER_AGENT  = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                                "Chrome/44.0.2403.157 Safari/537.36";
+const char* const TARGET_HOST = "www.azlyrics.com";
+const char* const URL_FMT     = "https://www.azlyrics.com/lyrics/%s/%s.html";
+const char* const OK_CODE     = "200";
 
-static void
-send_request (struct mg_connection *c)
+void
+send_request (struct mg_connection *c, char *s_artist, char *s_song)
 {
     mg_printf (c,
                "GET https://%s/lyrics/%s/%s.html HTTP/1.0\r\n"
                "Host: %s\r\n"
                "User-Agent: %s\r\n"
                "\r\n",
-               s_host, s_artist, s_song, s_host, s_user_agent);
+               TARGET_HOST, s_artist, s_song, TARGET_HOST, USER_AGENT);
 }
 
-static void
-fn (struct mg_connection *c, int ev, void *ev_data, void *fn_data)
+void
+cz_handle_response (void)
+{
+}
+
+void
+cz_write_to_file (void)
+{
+}
+
+void
+fn_spider (struct mg_connection *c, int ev, void *ev_data, void *fn_data)
 {
     if (ev == MG_EV_CONNECT)
     {
-        struct mg_tls_opts opts = {.srvname = s_host};
+        struct mg_tls_opts opts = {.srvname = TARGET_HOST};
         mg_tls_init(c, &opts);
-        send_request (c);
+        send_request (c, ((struct FnSpiderData *) fn_data)->s_artist, ((struct FnSpiderData *) fn_data)->s_song);
     }
     else if (ev == MG_EV_HTTP_MSG)
     {
-        struct mg_http_message          *hm;
-        hm = (struct mg_http_message *)  ev_data;
+        struct mg_http_message *hm;
+        char                   *response_code;
 
-        char *response_ok = "200";
-        char *response_code;
+        hm = (struct mg_http_message *) ev_data;
+
         strncpy (response_code, hm->uri.ptr, (size_t) hm->uri.len);
-        if (strcmp (response_ok, response_code) == 0)
+        if (strcmp (OK_CODE, response_code) == 0)
         {
             // Write html to file
-            char  buffer[1024];
-            snprintf (buffer, sizeof (buffer), "./cache/%s_%s.html", s_artist, s_song);
-            err = mg_file_printf (buffer, "%.*s", (int) hm->body.len, hm->body.ptr) == 0;
+            char buffer[1024];
+            snprintf (buffer,
+                      sizeof (buffer),
+                      "./cache/%s_%s.html",
+                      ((struct FnSpiderData *) fn_data)->s_artist,
+                      ((struct FnSpiderData *) fn_data)->s_song);
+            ((struct FnSpiderData *) fn_data)->cz_errno = mg_file_printf (buffer,
+                                                                          "%.*s",
+                                                                          (int) hm->body.len,
+                                                                          hm->body.ptr) == 0;
         }
         else
-            err = 1;
+            ((struct FnSpiderData *) fn_data)->cz_errno = 1;
 
         c->is_closing = 1;
-        *(bool *) fn_data = true;
+        ((struct FnSpiderData *) fn_data)->done = true;
     }
     else if (ev == MG_EV_ERROR)
     {
-        err = 1;
-        *(bool *) fn_data = true;
+        ((struct FnSpiderData *) fn_data)->cz_errno = 1;
+        ((struct FnSpiderData *) fn_data)->done = true;
     }
+}
+
+void
+free_fn_spider_data (struct FnSpiderData **data)
+{
+    free ((*data)->s_artist);
+    free ((*data)->s_song);
+    free (*data);
+}
+
+struct FnSpiderData *
+new_fn_spider_data (char *artist, char *song)
+{
+    struct FnSpiderData *data;
+
+    data = malloc (sizeof (struct FnSpiderData));
+    data->s_artist = strdup (artist);
+    data->s_song = strdup (song);
+    data->done = false;
+    data->cz_errno = 0;
+
+    return data;
 }
 
 int
 scrape_lyrics (const char *artist, const char *song)
 {
-    struct mg_mgr mgr;
-    s_artist = artist;
-    s_song = song;
-    char url[1024];
-    snprintf (url, 1024, "https://%s/lyrics/%s/%s.html", s_host, artist, song);
-    bool done = false;
+    struct mg_mgr        mgr;
+    char                 url[1024];
+    struct FnSpiderData *data;
+    int                  cz_errno;
+
+    data = new_fn_spider_data ((char *) artist, (char *) song);
+
+    snprintf (url, 1024, URL_FMT, artist, song);
+
     mg_log_set ("3");
     mg_mgr_init (&mgr);
-    mg_http_connect (&mgr, url, fn, &done);
-    while (!done)
+    mg_http_connect (&mgr, url, fn_spider, data);
+    while (!(data->done))
         mg_mgr_poll (&mgr, 1000);
     mg_mgr_free (&mgr);
-    return err;
+
+    cz_errno = data->cz_errno;
+    free_fn_spider_data (&data);
+
+    return cz_errno;
 }
--- src/czlyrics-spider.h
+++ src/czlyrics-spider.h
@@ -3,6 +3,25 @@
 
 #include "external/mongoose/mongoose.h"
 
+extern const char* const USER_AGENT;  /* User-Agent header for making requests to target site */
+extern const char* const TARGET_HOST; /* Hostname of target site */
+extern const char* const URL_FMT;     /* Target site URL format with placeholders for arguments */
+extern const char* const OK_CODE;     /* String format of 200 OK response code */
+
+struct FnSpiderData
+{
+    char *s_artist;
+    char *s_song;
+    bool  done;
+    int   cz_errno;
+} FnSpiderData; /* Contains arguments passed to fn_spider's fn_data param */
+
 int scrape_lyrics (const char *artist, const char *song);
+void fn_spider (struct mg_connection *c, int ev, void *ev_data, void *fn_data);
+void send_request (struct mg_connection *c, char *s_artist, char *s_song);
+struct FnSpiderData *new_fn_spider_data (char *artist, char *song);
+void free_fn_spider_data (struct FnSpiderData **data);
+void cz_handle_response (void);
+void cz_write_to_file (void);
 
 #endif /* CZLYRICS_SPIDER_H */
